b4161eb943d19afe975c1971621811a387145f0a
Tom Crane <tom@nanoplanet.com>
2014-04-01 09:16:27 +0100

    use dotnetrdf for better control of serialisation of LOC marc xml

--- MarcTools/Controllers/MarcController.cs
+++ MarcTools/Controllers/MarcController.cs
@@ -8,9 +8,13 @@ using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using MarcTools.Models;
+using Microsoft.Ajax.Utilities;
 using net.sf.saxon;
 using net.sf.saxon.trans;
 using SobekCM.Bib_Package.MARC;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Writing;
 using Controller = System.Web.Mvc.Controller;
 
 namespace MarcTools.Controllers
@@ -57,32 +61,69 @@ namespace MarcTools.Controllers
             var baseUri = "http://wellcomelibrary.org/bf/" + id + "/";
             var saxonXqy = Server.MapPath("~/lcnetdev/xbin/saxon.xqy");
             var record = GetMarcRecord(id);
+            
             var tempMarcXml = Path.GetTempFileName();
             record.Save_MARC_XML(tempMarcXml);
             var bibQuery = new BibFrameQuery();
-            var bf = bibQuery.DoBibFrameQuery(saxonXqy, tempMarcXml, baseUri, serialisation);
-            // dirty tidyup
-            if (serialisation == "ntriples" && bf.Contains("?>&lt;"))
-            {
-                bf = bf.Replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "");
-                bf = bf.Replace("&lt;", "<");
-                bf = bf.Replace("&gt;", ">");
-            }
-            if (serialisation == "json" && bf.Contains("&#xD;"))
-            {
-                bf = bf.Replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "");
-                bf = bf.Replace("&#xD;", "");
-                bf = bf.Replace("&lt;", "<");
-                bf = bf.Replace("&gt;", ">");
-            }
-            var cr = new ContentResult
-            {
-                Content = bf,
-                ContentType = serialisation.StartsWith("rdfxml") ? "text/xml" : "text/plain",
-                ContentEncoding = Encoding.UTF8
-            };
+            var bf = bibQuery.DoBibFrameQuery(saxonXqy, tempMarcXml, baseUri, "rdfxml");
             System.IO.File.Delete(tempMarcXml);
+
+            var cr = new ContentResult {ContentType = "text/plain"};
+            // need to give each serialisation its proper mime type
+
+            if (serialisation == "rdfxml")
+            {
+                cr.ContentEncoding = Encoding.UTF8;
+                cr.Content = bf;
+                cr.ContentType = "text/xml";
+                return cr;
+            }
+
+            // load into dotnetrdf as Loc is a bit wrong for other serialisations
+            var g = new Graph();
+            StringParser.Parse(g, bf);
+            IRdfWriter writer = null;
+            var sw = new System.IO.StringWriter();
+            switch (serialisation)
+            {
+                case "turtle" :
+                    writer = new CompressingTurtleWriter();
+                    break;
+                default:
+                    cr.Content = "invalid serialisation; use one of rdfxml, turtle";
+                    cr.ContentType = "text/plain";
+                    return cr;
+
+            }
+            if (writer is IPrettyPrintingWriter)
+            {
+                ((IPrettyPrintingWriter)writer).PrettyPrintMode = true;
+            }
+            writer.Save(g, sw);
+            cr.Content = sw.ToString();
             return cr;
+
+            // dirty tidyup
+            //if (serialisation == "ntriples" && bf.Contains("?>&lt;"))
+            //{
+            //    bf = bf.Replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "");
+            //    bf = bf.Replace("&lt;", "<");
+            //    bf = bf.Replace("&gt;", ">");
+            //}
+            //if (serialisation == "json" && bf.Contains("&#xD;"))
+            //{
+            //    bf = bf.Replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "");
+            //    bf = bf.Replace("&#xD;", "");
+            //    bf = bf.Replace("&lt;", "<");
+            //    bf = bf.Replace("&gt;", ">");
+            //}
+            ////var cr = new ContentResult
+            ////{
+            ////    Content = bf,
+            ////    ContentType = serialisation.StartsWith("rdfxml") ? "text/xml" : "text/plain",
+            ////    ContentEncoding = Encoding.UTF8
+            ////};
+            ////return cr;
         }
 
         private MARC_Record GetMarcRecord(String bNumber)
